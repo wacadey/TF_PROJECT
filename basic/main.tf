79064d9639a0d66fc5b1ae54218da34ac8c04442
wacadey <wacadey@gmail.com>
2026-07-27 13:53:18 +0900

    Feat: EIP 표시(주석처리) 한 기본 EC2 생성 코드

--- basic/main.tf
+++ basic/main.tf
@@ -14,84 +14,92 @@ data "aws_subnets" "default" {
 }
 
 # 3. 보안그룹 생성 선언 - EC2 진입 하는데 인바운드 IP/포트, 아웃바운드 IP/포트 설정 => 접근 제한!!
-# resource "aws_security_group" "DE-AI-12-IaC-TF-GROUP" {
-#   # 메타 정보
-#   name        = "terraform-12-sg"
-#   description = "de-ai-12 security group"
-#   # 보안 그룹은 VPC에 종속되어서 구성됨
-#   # id => 리소스명-해시값(중복x, 고유값)
-#   vpc_id = data.aws_vpc.default.id
-#   # 인바운드   (외부 트레픽이 내부로 들어옴) -> 일단 필요한 만큼 생성 -> 추후 반복문?등 문법효율적 활용을 통해 구성
-#   ingress {
-#     protocol    = "tcp"
-#     from_port   = 22
-#     to_port     = 22
-#     description = "SSH"
-#     # 0.0.0.0/0 => 각자리가 256개 표현(0~255).().().()/앞에서부터 고정값(비트수:0,8,16,24,32)
-#     # 256*256*256*256 개 주소 표현
-#     # 10.0.0.0/8 => 맨 앞에 1자리는 고정 => 10은 고정 나머지 3자리에서 모두 가능 => 256*256*256 주소 가능함
-#     # 0.0.0.0/0 => Anywhere IPV4 (전세계 어디서든 접근 가능 -> 보안에 좋진 않다)
-#     # 222.108.125.33/32 => 오직 이 IP만 접속 가는함!! ~/32(4자리 모두 고정)
-#     cidr_blocks = ["222.108.125.33/32"]
-#   }
-#   ingress { # HTTP 구성 실습
-#     protocol    = "tcp"
-#     from_port   = 80
-#     to_port     = 80
-#     description = "HTTP"
-#     cidr_blocks = ["0.0.0.0/0"] # 전세계로 개방
-#   }
-#   # 아웃바운드 (내부 트레픽이 외부로 나감)
-#   egress {
-#     # 모든 프로토콜 개발
-#     protocol = "-1"
-#     # 모든 포트 개방
-#     from_port = 0
-#     to_port   = 0
-#     # 전세계로 개방
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-# }
-
+resource "aws_security_group" "DE-AI-12-IaC-TF-GROUP" {
+  # 메타 정보
+  name = "terraform-12-sg2"
+  # ASCII만 지원
+  description = "de-ai-12 security group"
+  # 보안 그룹은 VPC에 종속되어서 구성됨
+  # id => 리소스명-해시값(중복x, 고유값)
+  vpc_id = data.aws_vpc.default.id
+  # 인바운드   (외부 트레픽이 내부로 들어옴) -> 일단 필요한 만큼 생성 -> 추후 반복문?등 문법효율적 활용을 통해 구성
+  ingress {
+    protocol    = "tcp"
+    from_port   = 22
+    to_port     = 22
+    description = "SSH"
+    # 0.0.0.0/0 => 각자리가 256개 표현(0~255).().().()/앞에서부터 고정값(비트수:0,8,16,24,32)
+    # 256*256*256*256 개 주소 표현
+    # 10.0.0.0/8 => 맨 앞에 1자리는 고정 => 10은 고정 나머지 3자리에서 모두 가능 => 256*256*256 주소 가능함
+    # 0.0.0.0/0 => Anywhere IPV4 (전세계 어디서든 접근 가능 -> 보안에 좋진 않다)
+    # 222.108.125.33/32 => 오직 이 IP만 접속 가는함!! ~/32(4자리 모두 고정)
+    cidr_blocks = ["222.108.125.33/32"]
+  }
+  ingress { # HTTP 구성 실습
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+    description = "HTTP"
+    cidr_blocks = ["0.0.0.0/0"] # 전세계로 개방
+  }
+  # 아웃바운드 (내부 트레픽이 외부로 나감)
+  egress {
+    # 모든 프로토콜 개발
+    protocol = "-1"
+    # 모든 포트 개방
+    from_port = 0
+    to_port   = 0
+    # 전세계로 개방
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
 # 아마존 리눅스 AMI의 ID 조회
 data "aws_ami" "amazon_linux" {
-    # 최신 설정
-    most_recent = true
-    # 소유자
-    owners = [ "amazon" ]
-    # 필터링
-    filter {
-      name = "name"
-      values = [ "al2023-ami-*" ]
-    }  
-    # 프리티어를 사용할려면 필터를 추가해야함 -> ec2에서 인스턴스 유형이 t2/t3.micro등 선택되어야 확정됨
-    # 필터 추가
-    filter {
-      name = "architecture"
-      values = [ "x86_64" ]
-    }
+  # 최신 설정
+  most_recent = true
+  # 소유자
+  owners = ["amazon"]
+  # 필터링
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*"]
+  }
+  # 프리티어를 사용할려면 필터를 추가해야함 -> ec2에서 인스턴스 유형이 t2/t3.micro등 선택되어야 확정됨
+  # 필터 추가
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
 }
 
 # 4. EC2 생성 선언
-resource "aws_instance" "DE-AI-25-IaC-TF" {
-    # AMI -> OS
-    ami = data.aws_ami.amazon_linux.id
-    # 인스턴스 유형
-    instance_type = var.instance_type
-    # 키 페어
-    key_name = var.key_name
-    # 서브넷
-    subnet_id = data.aws_subnets.default.ids[0] # a,b,c,d중 첫번재 선택(a)
-    # 보안그룹
-    vpc_security_group_ids = [
-        aws_security_group.DE-AI-25-IaC-TF-GROUP.id
-    ]
-    # 스토리지 생략
-    # 고급 설정 생략
-    # 태그
-    tags = {
-      Name = "DE-AI-25-IaC-TF-EC2"
-    }
-    # IP는 임시로 자동할당 (현재 EIP 사용 x)
+resource "aws_instance" "DE-AI-12-IaC-TF" {
+  # AMI -> OS
+  ami = data.aws_ami.amazon_linux.id
+  # 인스턴스 유형
+  instance_type = var.instance_type
+  # 키 페어
+  key_name = var.key_name
+  # 서브넷
+  subnet_id = data.aws_subnets.default.ids[0] # a,b,c,d중 첫번재 선택(a)
+  # 보안그룹
+  vpc_security_group_ids = [
+    aws_security_group.DE-AI-12-IaC-TF-GROUP.id
+  ]
+  # 스토리지 생략
+  # 고급 설정 생략
+  # 태그
+  tags = {
+    Name = "DE-AI-12-IaC-TF-EC2"
+  }
+  # IP는 임시로 자동할당 (현재 EIP 사용 x)
 }
+
+# 5. Elastic IP 생성 선언 -> EC2 연결 (차후 IP를 미리 확보하고 ec2 생성되면 연결하는 방식 진화)
+# resource "aws_eip" "DE-AI-12-IaC-TF-EIP" {
+#   # EC2 인스턴스 
+#   instance = aws_instance.DE-AI-12-IaC-TF.id
+#   # 네트워크
+#   domain = "vpc"
+# }
--- basic/outputs.tf
+++ basic/outputs.tf
@@ -1,25 +1,38 @@
-# 각종 정보 출력, 통상 terraform apply 수행의 결과를 출력하는 용도로 활용
-# EC2 구성이라면 -> EIP 값, 인스턴스값
+# # 각종 정보 출력, 통상 terraform apply 수행의 결과를 출력하는 용도로 활용
+# # EC2 구성이라면 -> EIP 값, 인스턴스값
 
-# 임시코드, vpc, subnets 출력
-output "default_vpc_id" {
-  value       = data.aws_vpc.default.id
-  description = "서울 리전의 기본 VPC의 id"
-}
-output "default_subnets_ids" {
-  value       = data.aws_subnets.default.ids
-  description = "서울 리전의 기본 VPC에 속한 서브넷 ID 리스트"
-}
+# # 임시코드, vpc, subnets 출력
+# output "default_vpc_id" {
+#   value       = data.aws_vpc.default.id
+#   description = "서울 리전의 기본 VPC의 id"
+# }
+# output "default_subnets_ids" {
+#   value       = data.aws_subnets.default.ids
+#   description = "서울 리전의 기본 VPC에 속한 서브넷 ID 리스트"
+# }
 
-# 현재 위치 확인 ( ~/basic/ )
-# 출결결과 확인 (init -> plan -> `apply` -> destroy)
+# # 현재 위치 확인 ( ~/basic/ )
+# # 출결결과 확인 (init -> plan -> `apply` -> destroy)
 
-# 앞파벳 순으로 출력된다 (작성 기준 X)
-output "aws_ami_amazon_linux_id" {
-  value       = data.aws_ami.amazon_linux.id
-  description = "아마존 리눅스 AMI 아이디 조회"
+# # 앞파벳 순으로 출력된다 (작성 기준 X)
+# output "aws_ami_amazon_linux_id" {
+#   value       = data.aws_ami.amazon_linux.id
+#   description = "아마존 리눅스 AMI 아이디 조회"
+# }
+# output "aws_ami_amazon_linux_info" {
+#   value       = data.aws_ami.amazon_linux
+#   description = "아마존 리눅스 AMI 조회"
+# }
+
+# EIP의 퍼블릭 IP 출력
+# output "public_ip" {
+#     value = aws_eip.DE-AI-25-IaC-TF-EIP.public_ip
+# }
+# Ec2 생성시 자동으로 부여되는 IP
+output "public_ip" {
+    value = aws_instance.DE-AI-25-IaC-TF.public_ip
 }
-output "aws_ami_amazon_linux_info" {
-  value       = data.aws_ami.amazon_linux
-  description = "아마존 리눅스 AMI 조회"
+# Ec2 인스턴스 ID 출력
+output "instance_id" {
+    value = aws_instance.DE-AI-25-IaC-TF.id
 }
