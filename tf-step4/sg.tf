777f1539fd559a5caca8f8a7bf21c74e2a4a76a2
wacadey <wacadey@gmail.com>
2026-07-29 10:33:39 +0900

    Feat: sg 리소스 구성 완료，Rule 구성 계획

--- tf-step4/sg.tf
+++ tf-step4/sg.tf
@@ -4,40 +4,76 @@
 locals {
   security_groups = {
     web = {
-        ingress = {
-            ssh   = {
-                port = 22,              # 포트 값 표현
-                cidr = ["0.0.0.0/0"]    # n개 나올수 있는 허가된 IP값 나열(리스트)
-            }
-            http  = {
-                port = 80,
-                cidr = ["0.0.0.0/0"]
-            }            
+      ingress = {
+        ssh = {
+          port = 22,           # 포트 값 표현
+          cidr = ["0.0.0.0/0"] # n개 나올수 있는 허가된 IP값 나열(리스트)
         }
+        http = {
+          port = 80,
+          cidr = ["0.0.0.0/0"]
+        }
+      }
     }
     was = {
-        ingress = {
-            ssh   = {
-                port = 22,              # 포트 값 표현
-                cidr = ["0.0.0.0/0"]    # n개 나올수 있는 허가된 IP값 나열(리스트)
-            }
-            tcp  = {
-                port = 8000,
-                cidr = ["0.0.0.0/0"]
-            }            
+      ingress = {
+        ssh = {
+          port = 22,           # 포트 값 표현
+          cidr = ["0.0.0.0/0"] # n개 나올수 있는 허가된 IP값 나열(리스트)
         }
+        # 오직 web -> was로만 접근해야함. 취지 벗어남 (다른 방식으로 구성)
+        # tcp  = {
+        #     port = 8000,
+        #     cidr = ["0.0.0.0/0"]
+        # }            
+      }
     }
-    db  = {
-        ingress = {
-            ssh   = {
-                port = 22,              # 포트 값 표현
-                cidr = ["0.0.0.0/0"]    # n개 나올수 있는 허가된 IP값 나열(리스트)
-            }
-            tcp  = {
-                port = 3306,
-                cidr = ["0.0.0.0/0"]
-            }            
+    db = {
+      ingress = {
+        ssh = {
+          port = 22,           # 포트 값 표현
+          cidr = ["0.0.0.0/0"] # n개 나올수 있는 허가된 IP값 나열(리스트)
         }
+        # 오직 was -> db로만 접근해야함. 취지 벗어남 (다른 방식으로 구성)
+        # tcp  = {
+        #     port = 3306,
+        #     cidr = ["0.0.0.0/0"]
+        # }            
+      }
     }
+  }
+}
+
+############################################
+# Security Groups 생성 선언
+############################################
+resource "aws_security_group" "sg" {
+  # 반복 데이터로 구성한 locals 주입(세팅) -> web, was, db 총 3개의 맴버를 가짐
+  for_each = local.security_groups
+  # for_each에 Map 타입으로 주입 => each.key, each.value 형태로 반복적, 순서대로 획득 
+  # 이름 => DE-AI-12-web-sg-해시값, DE-AI-12-was-sg-해시값, DE-AI-12-db-sg-해시값
+  name_prefix = "DE-AI-12-${each.key}-sg-"
+  description = "${upper(each.key)} Security Group"
+
+  # VPC 지정
+  vpc_id = aws_vpc.DE-AI-12-company.id
+
+  # ingress 동적 생성 => 블록 반복 구성 => dynamic ingress
+  dynamic "ingress" {
+    for_each = each.value.ingress
+    content {
+      from_port   = ingress.value.port # 80, 22
+      to_port     = ingress.value.port # 80, 22
+      protocol    = "tcp"              # 고정값
+      cidr_blocks = ingress.value.cidr # ["0.0.0.0/0"]
+    }
+  }
+
+  # 아웃바운드 고정
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
---
+++ tf-step4/vpc.tf
@@ -0,0 +1,54 @@
+# 특정 기업/개인/단체등 전용 VPC 생성 선언
+resource "aws_vpc" "DE-AI-12-company" {
+  # CIDR(Classless Inter-Domain Routing) 규칙 지정 65536개 IP를 구성할수 있다. 10.0.0.0/16
+  # CIDR 블록 크기는 /16에서 /28 => AWS 제약사항
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  tags = {
+    Name = "DE-AI-12-company vpc"
+  }
+}
+# 서브넷 (public)
+resource "aws_subnet" "public" {
+  # 암묵적 의존성 -> 서브넷 구성을위해서는 반드시 vpc가 먼저 생성되어야 함
+  vpc_id = aws_vpc.DE-AI-12-company.id
+  # CIDR 가용영역 설정, VPC보다 작게, 24(3자리 고정) -> 256개 가용
+  cidr_block = "10.0.1.0/24"
+  # 리전마다 가용영역이 a,b,c,d  or a,b,c 제한 => 데이터센터 동수
+  availability_zone = "ap-northeast-2a"
+  # map 타입으로 관리 public_ip
+  map_public_ip_on_launch = true
+  # 개인 구분용도 일단 활용
+  tags = {
+    Name = "DE-AI-12-public-subnet"
+  }
+}
+# 현재까지는 퍼블릭 IP 활성화 차단된 상태임
+
+# 인터넷 게이트웨이
+resource "aws_internet_gateway" "company" {
+  vpc_id = aws_vpc.DE-AI-12-company.id
+  tags = {
+    Name = "DE-AI-12-company-igw"
+  }
+}
+
+# 라우트 테이블
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.DE-AI-12-company.id
+  # 모든 IP 대역 (편의상) => IGW 전달(연결)
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.company.id
+  }
+  tags = {
+    Name = "DE-AI-12-company-public-rt"
+  }
+}
+
+# 최종연결 (공개용 서브넷 -> 공개용 라우트테이블)
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
