439f366801e796833def596e96aefc855f22dced
wacadey <wacadey@gmail.com>
2026-07-31 15:24:50 +0900

    보안그룹 코멘트, 인바운드의 보안그룹 지정->트레픽 이동경로가 확인됨

--- tf-step5-v1/sg.tf
+++ tf-step5-v1/sg.tf
@@ -1,3 +1,6 @@
+###############################################
+# 퍼블릭 ALB 보안 그룹, 80포트 인바운드, 모든IP/port 아웃바운드, SSM사용으로 인해 22 포트제외
+###############################################
 resource "aws_security_group" "public_alb" {
   name        = "${local.project}-PUBLIC-ALB-SG"
   description = "Allow internet HTTP traffic"
@@ -17,6 +20,9 @@ resource "aws_security_group" "public_alb" {
   }
   tags = { Name = "${local.project}-PUBLIC-ALB-SG" }
 }
+###############################################
+# WEB ASG 보안그룹
+###############################################
 resource "aws_security_group" "web" {
   name        = "${local.project}-WEB-SG"
   description = "Allow HTTP only from public ALB"
@@ -26,6 +32,7 @@ resource "aws_security_group" "web" {
     from_port       = 80
     to_port         = 80
     protocol        = "tcp"
+    # cidr x, 보안그룹 -> public ALB 보안그룹 지정
     security_groups = [aws_security_group.public_alb.id]
   }
   egress {
@@ -36,6 +43,9 @@ resource "aws_security_group" "web" {
   }
   tags = { Name = "${local.project}-WEB-SG" }
 }
+###############################################
+# Internal ALB : 인바운드 8000, 아웃바운드 상동
+###############################################
 resource "aws_security_group" "internal_alb" {
   name        = "${local.project}-INTERNAL-ALB-SG"
   description = "Allow WAS traffic only from WEB tier"
@@ -45,6 +55,7 @@ resource "aws_security_group" "internal_alb" {
     from_port       = 8000
     to_port         = 8000
     protocol        = "tcp"
+    # cidr x, 보안그룹 -> web 보안그룹 지정
     security_groups = [aws_security_group.web.id]
   }
   egress {
@@ -55,6 +66,9 @@ resource "aws_security_group" "internal_alb" {
   }
   tags = { Name = "${local.project}-INTERNAL-ALB-SG" }
 }
+###############################################
+# WAS ASG 보안그룹
+###############################################
 resource "aws_security_group" "was" {
   name        = "${local.project}-WAS-SG"
   description = "Allow application traffic only from internal ALB"
@@ -64,6 +78,7 @@ resource "aws_security_group" "was" {
     from_port       = 8000
     to_port         = 8000
     protocol        = "tcp"
+    # cidr x, 보안그룹 -> internal_alb 보안그룹 지정
     security_groups = [aws_security_group.internal_alb.id]
   }
   egress {
@@ -74,6 +89,9 @@ resource "aws_security_group" "was" {
   }
   tags = { Name = "${local.project}-WAS-SG" }
 }
+###############################################
+# RDS 보안그룹
+###############################################
 resource "aws_security_group" "rds" {
   name        = "${local.project}-RDS-SG"
   description = "Allow MySQL only from WAS tier"
@@ -83,6 +101,7 @@ resource "aws_security_group" "rds" {
     from_port       = 3306
     to_port         = 3306
     protocol        = "tcp"
+    # cidr x, 보안그룹 -> was 보안그룹 지정
     security_groups = [aws_security_group.was.id]
   }
   egress {
